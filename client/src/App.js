fdf1111b6ec2f54d1ab486cb98cd4e476b766c49
Luis <luismfalconi@gmail.com>
2022-08-31 18:51:44 -0300

    feat(client/App.js): add Routes /,/products,,/products/:id

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import SearchBar from './components/SearchBar.jsx';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import Catalog from './components/Catalog';
+import CardDetail from './components/CardDetail';
 
 function App() {
-  //test commit remote
+  
   return (
+
     <div>
-      <SearchBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Catalog />} />
+        <Route exact path="/products/:id" element={<CardDetail />} />
+      </Routes>
     </div>
     
   );
