ff0010137c5b312d95e72931506f91967a86497a
MKamui <marioelkamui@gmail.com>
2022-08-31 19:34:22 -0500

    fix(client/card): eliminate data

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -8,18 +8,18 @@ const Card = ({shoe}) => {
           <div className="group relative cursor-pointer">
             <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-xl group-hover:opacity-60">
               <img
-                src="https://images.stockx.com/images/adidas-Yeezy-Foam-RNNR-Sand-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&trim=color&q=90&dpr=2&updated_at=1617991585"
-                alt='x'
+                src={shoe.image}
+                alt={shoe.name}
                 className="h-full w-full"
               />
             </div>
             <div className="mt-4 flex justify-between">
               <div>
-                <h3 className="text-xl text-bold text-[#00ff01]">Prueba</h3>
-                <p className="mt-1 text-md text-white">Marca</p>
-                <p className="mt-1 text-md text-white">White</p>
+                <h3 className="text-xl text-bold text-[#00ff01]">{shoe.name}</h3>
+                <p className="mt-1 text-md text-white">{shoe.brand}</p>
+                <p className="mt-1 text-md text-white">{shoe.color}</p>
               </div>
-              <p className="text-3xl font-medium p-4 text-white">$50</p>              
+              <p className="text-3xl font-medium p-4 text-white">${shoe.price}</p>              
             </div>
           </div>
         </div>
