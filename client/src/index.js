87887a4a515dddadbd69f14b8cd68a62fc12e92a
Luis <luismfalconi@gmail.com>
2022-09-01 10:52:47 -0300

    feat(src/redux y src/index.js): add redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ client/src/redux/actions.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export const GET_SHOES = 'GET_SHOES'
+export const GET_SHOE = 'GET_SHOE'
+export const GET_BY_NAME = 'GET_BY_NAME'
+
+export const getShoes = () => (dispatch)=>{
+    return axios('http://localhost:3001/shoes')
+                    .then(res => dispatch({type: 'GET_SHOES', payload: res.data}))
+}
+
+export const getShoe = (id) => (dispatch)=>{
+    return axios(`http://localhost:3001/shoes/${id}`)
+                    .then(res => dispatch({type: 'GET_SHOE', payload: res.data}))
+}
+
+export const getByName = (name) => {
+    return axios(`http://localhost:3001/shoes?name=${name}`)
+                    .then(res => dispatch({type: 'GET_BY_NAME', payload: res.data})) 
+}
