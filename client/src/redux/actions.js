bbe97b91917cc1338d8ca1fd6db9ab535aac012a
Luis <luismfalconi@gmail.com>
2022-09-02 16:27:54 -0300

    refactor(redux/actions): refactor route heroku-back

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -7,22 +7,22 @@ export const GET_BY_NAME = 'GET_BY_NAME'
 export const GET_BY_BRAND = 'GET_BY_BRAND'
 
 export const getShoes = () => (dispatch)=>{
-    return axios('http://localhost:3001/shoes')
+    return axios('https://sneakers-backend-henry.herokuapp.com/shoes')
                     .then(res => dispatch({type: 'GET_SHOES', payload: res.data}))
 }
 
 export const getShoe = (id) => (dispatch)=>{
-    return axios(`http://localhost:3001/shoes/${id}`)
+    return axios(`https://sneakers-backend-henry.herokuapp.com/shoes/${id}`)
                     .then(res => dispatch({type: 'GET_SHOE', payload: res.data}))
 }
 
 export const getBrands = () => (dispatch)=>{
-    return axios(`http://localhost:3001/brands`)
+    return axios(`https://sneakers-backend-henry.herokuapp.com/brands`)
                     .then(res => dispatch({type: 'GET_BRANDS', payload: res.data})) 
 }
 
 export const getByName = (name) => (dispatch)=>{
-    return axios(`http://localhost:3001/shoes?name=${name}`)
+    return axios(`https://sneakers-backend-henry.herokuapp.com/shoes?name=${name}`)
                     .then(res => dispatch({type: 'GET_BY_NAME', payload: res.data})) 
 }
 
