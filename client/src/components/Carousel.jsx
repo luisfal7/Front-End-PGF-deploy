6d726c9272e33348a5773af3acf01db227ce6aea
MKamui <marioelkamui@gmail.com>
2022-09-02 14:32:57 -0500

    feat(client/Carousel): Carousel component

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import { BsArrowLeftSquareFill, BsArrowRightSquareFill } from 'react-icons/bs'
+
+const sliderImg = [
+  {
+    url: 'http://desempacados.com/wp-content/uploads/2016/01/sneaker-coleccion-basicos-desempacados.jpg'
+  },
+  {
+    url: 'https://i.pinimg.com/474x/31/c4/d4/31c4d4f33c4836088956fcf9c3960d79.jpg'
+  },
+  {
+    url: 'https://www.allcitycanvas.com/wp-content/uploads/2018/12/Air-Jordan-Union-1200-1.jpg'
+  }
+]
+
+const Carousel = () => {
+  const [slide, setSlide] = useState(0)
+  const length = sliderImg.length
+
+  const prevSlide = () => {
+    setSlide(slide === length - 1 ? 0 : slide + 1)
+  }
+  const nextSlide = () => {
+    setSlide(slide === 0 ? length - 1 : slide -1)
+  }
+  
+  return (
+    <div className='px-4 py-16 relative flex justify-center items-center bg-black'>
+      <BsArrowLeftSquareFill onClick={prevSlide} className='absolute top-[50%] text-4xl text-[#00ff01] cursor-pointer left-8'/>
+      <BsArrowRightSquareFill onClick={nextSlide} className='absolute top-[50%] text-4xl text-[#00ff01] cursor-pointer right-8'/>
+      {sliderImg.map((item, index) => (
+        <div className={index === slide ? 'opacity-100' : 'opacity-0'}>
+          {index === slide && (<img className='w-full rounded-lg' src={item.url} alt="x" />)}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Carousel
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import Catalog from "./Catalog";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import Carousel from "./Carousel";
 
 function Home() {
     return ( 
     <div>
         <NavBar/>
+        <Carousel/>
         <Catalog/>
         <Footer />
     </div> 
