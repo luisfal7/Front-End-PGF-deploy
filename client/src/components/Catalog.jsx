072c48b734930e5287e54a82326c27e7ab2e9745
Luis <luismfalconi@gmail.com>
2022-09-02 12:48:17 -0300

    refactor(components/Catalog): add paginate

--- client/src/components/Catalog.jsx
+++ client/src/components/Catalog.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './Card';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
@@ -8,23 +8,58 @@ function Catalog(){
   const dispatch = useDispatch()
 
   let shoes = useSelector(state => state.filter);
-  console.log(shoes);
+
+  const [currentPage, setCurrentPage] = useState(0)
+
+  useEffect(()=>{
+    if(shoes.length){
+        setCurrentPage(0)
+    }
+  },[shoes])
+
+  const prevPage = ()=>{
+    if(currentPage < 9){
+        setCurrentPage(0)
+    }else{
+        setCurrentPage(currentPage - 8)
+    }
+  }
+
+  const nextPage = ()=>{
+    if(shoes.length <= currentPage + 8){
+        setCurrentPage(currentPage)
+    }else{
+        setCurrentPage(currentPage + 8)
+    }
+  }
+
+  const shoesPage = shoes.slice(currentPage, currentPage + 8)
 
   useEffect(()=>{
     dispatch(getShoes())
   },[dispatch])
+
   return (
     <div className="bg-black">
       <div className="text-[#00ff01] text-6xl font-bold text-center py-2">Products</div>
       <div className="container">
           <div className="mt-0 grid grid-cols-4 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {shoes.map(e => 
-            <Card 
-            key= {e._id} 
-            id = {e._id}
-            shoe= {e}
-            />)}
+            {shoesPage.map(e => 
+              <Card 
+                key= {e._id} 
+                id = {e._id}
+                shoe= {e}
+              />
+            )}
           </div>
+      </div>
+      <div className='flex justify-around'>
+        <button className='m-2' onClick={prevPage}>
+          Anterior
+        </button>
+        <button className='m-2' onClick={nextPage}>
+          siguiete
+        </button>
       </div>
     </div>
     
