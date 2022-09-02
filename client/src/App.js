34451edfe195974c911f460f27601c6bde2bb165
Hellsh4rk <yahir.lope@icloud.com>
2022-09-02 11:28:06 -0500

    refactor(app.js): add component footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Catalog from './components/Catalog';
 import CardDetail from './components/CardDetail';
+import Footer from './components/Footer';
 
 function App() {
   
   return (
 
     <div>
+      <Footer />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/products" element={<Catalog />} />
