e965a5d3dec370c952e4cc14f96cc21d65719544
Luis <luismfalconi@gmail.com>
2022-09-03 10:27:49 -0300

    Co-authored-by: juan bautista <jbcomas@users.noreply.github.com>

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,46 +3,40 @@ import SearchBar from "./SearchBar";
 import { BsCart, BsPerson } from "react-icons/bs";
 
 function NavBar() {
-    return ( 
-        <div className="">
-            <nav className="w-full bg-white">
-                <div className="flex justify-center">
-                    <div className="flex w-full items-center">
-                        <a className="mr-auto ml-0" href="#">Sneaker Paradise</a>
-                            <div className="justify-center items-center mr-auto">
-                                <ul className="flex">
-                                    <li className="flex">
-                                        <SearchBar/>
-                                    </li>
-                                    <li className="flex">
-                                        <a className="nav-link" aria-current="page" href="#">
-                                            <span className="text-red-600">OFERTAS</span>
-                                        </a>
-                                    </li>
-                                    <li className="flex"> 
-                                        <FilterBrand/>
-                                    </li>
-                                </ul>
-                            </div>
-                    </div>
-                    <div className="flex w-full items-center mr-auto">
-                        <ul className="flex">
-                            <li className="flex"> 
-                                <a className="nav-link" aria-current="page" href="#">
-                                    <span><BsCart/></span>
-                                </a>
-                            </li>
-                            <li className="flex">
-                                <a className="nav-link" aria-current="page" href="#">
-                                    <span><BsPerson/></span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <div className="container">
+      <nav className="flex justify-content: space-between items-center bg-white p-6">
+        <div className="flex items-center flex-shrink-0 mr-6">
+          <a className="fill current h-8 w-8 mr-2" href="#">
+            IMAGEN
+          </a>
+          <span className="font-semibold text-xl tracking-tight">Sneaker Paradise</span>
         </div>
-     );
+        <div className="flex justify-content-center text-center lg:flex lg:items-center lg:w-auto">
+            <a href="#"
+               className="block mt-4 lg:inline-block lg:mt-0 text-red-600 hover:text-red-800 mr-4">
+                OFERTAS
+            </a>
+            <div className="block mt-4 lg:inline-block lg:mt-0 mr-4">
+                <FilterBrand />
+            </div>    
+        </div>
+        <div className="flex justify-content-end lg:flex lg:items-center lg:w-auto">
+            <div className="block mt-4 lg:inline-block lg:mt-0 mr-4">      
+                < SearchBar />
+            </div>
+            <a href="#"
+                className="mt-4 flex text-center lg:inline-block lg:mt-0 hover:text-green-400 mr-4">
+                  <BsCart />
+            </a>
+            <a href="#"
+                className="block mt-4 lg:inline-block lg:mt-0 hover:text-green-400 mr-4">
+                  <BsPerson />
+            </a>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
