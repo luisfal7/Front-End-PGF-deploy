e54c804f1e72c75d0b7937b00785296c3dc2c7d4
Luis <luismfalconi@gmail.com>
2022-09-04 20:50:08 -0300

    refactor(components/NavBar): add button search and modif. ClassName

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,15 +5,15 @@ import { GiRunningShoe } from 'react-icons/gi'
 
 function NavBar() {
     return ( 
-        <div className="px-2 flex justify-between items-center w-full h-full bg-white">
-            <div className="flex items-center">
-                <h1 className="text-3xl font-bold mr-4 sm:text-4xl">Sneaker Paradise</h1>
-                <ul className="flex">
-                    <li><SearchBar/></li>
+        <div className="px-2 flex justify-between content-center items-center w-full h-full bg-white">
+            <h1 className="text-3xl font-bold mr-4 sm:text-4xl">Sneaker Paradise</h1>
+            <div className="flex justify-between items-center">
+                <ul>
                     <li><FilterBrand/></li>
                 </ul>
             </div>
             <div className="flex pr-4">
+                <SearchBar/>
                 <button className="text-black bg-red-600 border-none px-8 py-3 flex mr-4 text-xl"><GiRunningShoe className="text-3xl"/>Deals!</button>
                 <button className="mr-4 px-8 py-3 text-xl"><BsFillCartFill/></button>
                 <button className="px-8 py-3 text-xl"><BsPersonFill/></button>   
