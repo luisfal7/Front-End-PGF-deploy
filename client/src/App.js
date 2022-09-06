5ee954746adfdc7b663c0cbf880b2d8be3bde6df
MKamui <marioelkamui@gmail.com>
2022-09-06 17:22:59 -0500

    refactor(client/App, Navbar): include new components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Catalog from './components/Catalog';
 import CardDetail from './components/CardDetail';
+import CreateAccount from './components/CreateAccount';
+import Account from './components/Account'
+import Signin from './components/Signin'
 
 function App() {
   
@@ -13,6 +16,9 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/products" element={<Catalog />} />
         <Route exact path="/products/:id" element={<CardDetail/>} />
+        <Route exact path="/signin" element={<Signin/>} />
+        <Route exact path="/createaccount" element={<CreateAccount/>} />
+        <Route exac path='/account' element={<Account/>} />
       </Routes>
     </div>
     
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,14 +6,14 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
     return ( 
-        <div className="px-2 flex justify-between content-center items-center w-full h-full bg-white">
-            <Link to='/'><h1 className="text-3xl font-bold mr-4 sm:text-4xl">Sneaker Paradise</h1></Link>
+        <div className="px-2 flex justify-between content-center items-center w-full h-full bg-black">
+            <Link to='/'><h1 className="text-3xl font-bold mr-4 sm:text-4xl text-white">Sneaker Paradise</h1></Link>
             <div className="flex pr-4 items-center">
                 <div><FilterBrand/></div>
                 <div className="px-8 py-3 text-xl"><SearchBar/></div>
                 <button className="text-black bg-red-600 border-none px-8 py-3 flex mr-4 text-xl"><GiRunningShoe className="text-3xl"/>Deals!</button>
                 <button className="mr-4 px-8 py-3 text-xl"><BsFillCartFill/></button>
-                <button className="px-8 py-3 text-xl"><BsPersonFill/></button>   
+                <Link to='/signin'><button className="px-8 py-3 text-xl"><BsPersonFill/></button></Link>
             </div>
         </div>
     );
