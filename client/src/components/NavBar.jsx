fbe84c840ca294343d8a1cb8efb1055e51f1af01
MKamui <marioelkamui@gmail.com>
2022-09-06 18:15:00 -0500

    refactor(client/CatalogBrand, FilterBrand, Navbar): change style

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,20 +6,16 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
     return ( 
-        <div className="px-2 flex justify-between content-center items-center w-full h-full bg-white">
+        <div className="px-2 flex justify-between content-center items-center w-full h-full bg-black">
             <a href="/">
-            <h1 className="text-3xl font-bold mr-4 sm:text-4xl">Sneaker Paradise</h1>
+            <h1 className="text-3xl font-bold mr-4 sm:text-4xl text-white">Sneaker Paradise</h1>
             </a>
-            <div className="flex justify-between items-center">
-                <ul>
-                    <li><FilterBrand/></li>
-                </ul>
-            </div>
             <div className="flex pr-4">
+                <FilterBrand/>
                 <SearchBar/>
                 <button className="text-black bg-red-600 border-none px-8 py-3 flex mr-4 text-xl"><GiRunningShoe className="text-3xl"/>Deals!</button>
                 <button className="mr-4 px-8 py-3 text-xl"><BsFillCartFill/></button>
-                <Link to='/signin'><button className="px-8 py-3 text-xl"><BsPersonFill/></button></Link>
+                <Link to='/signin'><button className="px-8 py-4 text-xl"><BsPersonFill/></button></Link>
             </div>
         </div>
     );
