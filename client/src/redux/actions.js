631517ff6bd58dfce56f758a96ca2f9835b44938
Luis <luismfalconi@gmail.com>
2022-09-06 15:53:58 -0300

    refactor(redux/actions): add action getByCatalogBrand

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -5,6 +5,7 @@ export const GET_SHOE = 'GET_SHOE'
 export const GET_BRANDS = 'GET_BRANDS'
 export const GET_BY_NAME = 'GET_BY_NAME'
 export const GET_BY_BRAND = 'GET_BY_BRAND'
+export const GET_BY_CATALOG_BRAND = 'GET_BY_CATALOG_BRAND'
 
 export const getShoes = () => (dispatch)=>{
     return axios('https://sneakers-backend-henry.herokuapp.com/shoes')
@@ -26,6 +27,10 @@ export const getByName = (name) => (dispatch)=>{
                     .then(res => dispatch({type: 'GET_BY_NAME', payload: res.data})) 
 }
 
+export const getByCatalogBrand = (brand) => (dispatch)=>{
+    return axios(`https://sneakers-backend-henry.herokuapp.com/shoes?brand=${brand}`)
+                    .then(res => dispatch({type: 'GET_BY_CATALOG_BRAND', payload: res.data})) 
+}
 
 export const getByBrand = (brand) =>{
     return {
