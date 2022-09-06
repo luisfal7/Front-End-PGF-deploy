561bc87693b9a511b0f269f2bd477a05727d68dc
Luis <luismfalconi@gmail.com>
2022-09-06 15:49:28 -0300

    refactor(src/App): add route colletions/:brands

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Catalog from './components/Catalog';
 import CardDetail from './components/CardDetail';
+import CatalogBrand from './components/CatalogBrand';
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/products" element={<Catalog />} />
         <Route exact path="/products/:id" element={<CardDetail/>} />
+        <Route exact path="/colletions/:brand" element={<CatalogBrand/>} />
       </Routes>
     </div>
     
