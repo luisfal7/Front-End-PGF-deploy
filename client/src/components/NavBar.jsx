58d4cfbd0fb9597203ff4f9a34b504f60984391b
Luis <luismfalconi@gmail.com>
2022-09-09 14:50:47 -0300

    refactor(): add Link

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,15 @@
 import FilterBrand from "./FilterBrand";
 import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
 import { BsFillCartFill, BsPersonFill } from "react-icons/bs";
 import { GiRunningShoe } from 'react-icons/gi'
-import { Link } from "react-router-dom";
 
 function NavBar() {
     return ( 
         <div className="px-2 flex justify-between content-center items-center w-full h-full bg-white">
-            <a href="/">
+            <Link to="/">
             <h1 className="text-3xl font-bold mr-4 sm:text-4xl">Sneaker Paradise</h1>
-            </a>
+            </Link>
             <div className="flex justify-between items-center">
                 <ul>
                     <li><FilterBrand/></li>
@@ -18,7 +18,9 @@ function NavBar() {
             <div className="flex pr-4">
                 <SearchBar/>
                 <button className="text-black bg-red-600 border-none px-8 py-3 flex mr-4 text-xl"><GiRunningShoe className="text-3xl"/>Deals!</button>
-                <button className="mr-4 px-8 py-3 text-xl"><BsFillCartFill/></button>
+                <Link to={`/cart`}>
+                    <button className="mr-4 px-8 py-3 text-xl"><BsFillCartFill/></button>
+                </Link>
                 <button className="px-8 py-3 text-xl"><BsPersonFill/></button>   
             </div>
         </div>
