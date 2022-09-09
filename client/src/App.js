c70b70332203240aabb42fc87839fc11d5139930
Luis <luismfalconi@gmail.com>
2022-09-09 14:34:37 -0300

    refactor(src/App): add Cart

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Catalog from './components/Catalog';
 import CardDetail from './components/CardDetail';
 import CatalogBrand from './components/CatalogBrand';
+import Cart from './components/Cart';
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/products" element={<Catalog />} />
         <Route exact path="/products/:id" element={<CardDetail/>} />
         <Route exact path="/colletions/:brand" element={<CatalogBrand/>} />
+        <Route exact path="/cart" element={<Cart/>} />
       </Routes>
     </div>
     
