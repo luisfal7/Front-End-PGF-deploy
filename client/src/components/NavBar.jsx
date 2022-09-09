13cba9c586d9b25e2c296d99bec6d8aa02679aa4
Luis <luismfalconi@gmail.com>
2022-09-09 18:27:07 -0300

    refactor(components/NavBar): add link button cart

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -7,14 +7,14 @@ import { Link } from "react-router-dom";
 function NavBar() {
     return ( 
         <div className="px-2 flex justify-between content-center items-center w-full h-full bg-black">
-            <a href="/">
+            <Link to="/">
             <h1 className="text-3xl font-bold mr-4 sm:text-4xl text-white">Sneaker Paradise</h1>
-            </a>
+            </Link>
             <div className="flex pr-4">
                 <FilterBrand/>
                 <SearchBar/>
                 <button className="text-black bg-red-600 border-none px-8 py-3 flex mr-4 text-xl"><GiRunningShoe className="text-3xl"/>Deals!</button>
-                <button className="mr-4 px-8 py-3 text-xl"><BsFillCartFill/></button>
+                <Link to='/cart'><button className="mr-4 px-8 py-4 text-xl"><BsFillCartFill/></button></Link>
                 <Link to='/account'><button className="px-8 py-4 text-xl"><BsPersonFill/></button></Link>
             </div>
         </div>
