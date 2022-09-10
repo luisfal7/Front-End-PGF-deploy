642acce9917506145f3e2c572d56b80b9da83ded
Luis <luismfalconi@gmail.com>
2022-09-10 20:26:33 -0300

    refactor(components/Card): edit component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import Select from "react-select";
+import Swal from "sweetalert2";
 import {addProductCarrito} from '../redux/actions/actions'
 
 
 const Card = ({shoe}) => {
   const dispatch = useDispatch()
 
-  const cart = ()=>{
-    dispatch(addProductCarrito(shoe))
+  const [shoeToCart, setShoeToCart] = useState({
+    _id:'',
+    name:'',
+    image:'',
+    quantity:0,
+    price:null,
+    size:null,
+  })
+
+  const options = shoe.stock?.map(e => ({value:e._id, label:e.size}))
+
+   const selectSize = (e)=>{
+    setShoeToCart({
+      _id:shoe._id,
+      name: shoe.name,
+      image:shoe.image,
+      quantity:1,
+      price:shoe.price,
+      size:e.label
+    })
+  }
+ 
+  const toCart = (e)=>{
+    if(shoeToCart.quantity === 0){
+      e.preventDefault()
+      Swal.fire({
+        position: 'top-center',
+        icon: 'error',
+        title: 'Add size',
+        showConfirmButton: false,
+        timer: 3000
+      })
+      
+    }else{
+      dispatch(addProductCarrito(shoeToCart))
+      Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Your shoe add to cart',
+        showConfirmButton: false,
+        timer: 2000
+      })
+    }
   }
 
   return (
@@ -24,16 +67,19 @@ const Card = ({shoe}) => {
                 className="object-contain h-38 w-52 bg-black rounded-lg"
               />
             </div>
-            <div className="mt-2 flex justify-between">
+            <div className="mt-1 flex justify-between">
               <div>
                 <p className="mt-1 text-sm text-white uppercase">{shoe.brand}</p>
                 <h3 className="text-sm text-bold text-[#00ff01] capitalize">{shoe.name}</h3>
-                <p className="mt-1 text-md text-white">${shoe.price}</p>
+                <p className="my-1 text-md text-white">${shoe.price}</p>
               </div>             
             </div>
           </div>
           </Link>
-          <button className="mt-1 text-md" onClick={cart}>BUY ME!</button>
+          <div className='mt-1 flex flex-row mx-auto items-center justify-between'>
+            <button className="p-1.5 text-md" onClick={toCart}>BUY ME!</button>
+            <Select options={options} placeholder="size" onChange={selectSize}/>
+          </div>
         </div>
       </div>
     </div> 
