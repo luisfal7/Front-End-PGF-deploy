1fee258293bc12803a763307831484f46174a87e
Luis Falconi <luism.falconi@gmail.com>
2022-09-13 19:14:59 -0300

    c4

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     return ( 
     <div>
         <NavBar/>
-        <h1 className="flex items-center">vercel 2</h1>
+        <h1 className="flex items-center">vercel 3</h1>
         <Carousel/>
         <Catalog/>
         <Footer />
